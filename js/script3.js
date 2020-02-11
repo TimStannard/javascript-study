d94a35039a66124f58219171e47a8a6a4470f81a
Unknown <stannard.tim@gmail.com>
2020-02-11 19:57:03 +1300

    Finished exercise in printing complex objects
    Yay

--- js/script3.js
+++ js/script3.js
@@ -10,8 +10,44 @@ var ourMusic = [
       "LP"
     ],
     "gold": true
+  },
+  {
+    "artist": "Jon Jones",
+    "title": "GOAT",
+    "release_year": 2020,
+    "formats": [ 
+      "CD", 
+      "USB Stick", 
+      "LP"
+    ],
+    "gold": false
+  },
+   {
+    "artist": "Chael Sonnen",
+    "title": "Even more of A GOAT",
+    "release_year": 2021,
+    "formats": [ 
+      "Floppy Disk", 
+      "LP"
+    ],
+    "gold": false
   }
 ];
 
-document.getElementById("record").innerHTML = "<h2>Record</h2><br>" + "<strong>Artist </strong>" + ourMusic[0].artist + "<br>" +
-                                              "<strong>Title </strong>" + ourMusic[0].title + "<br>";
+
+
+function printOurMusic(){
+  var i;
+  for (i = 0; i < ourMusic.length; i++) {
+    // add spaces after commas in the formats 
+    ourMusic[i].formats = ourMusic[i].formats.toString();
+    ourMusic[i].formats = ourMusic[i].formats.replace(/,/g, ", ");
+    // show all music loop
+    document.getElementById("record").innerHTML += "<div class='box'><h3>Record "+ (i+1) + "</h3><br>" + "<strong>Artist </strong>" + ourMusic[i].artist +
+                                              "<br><strong>Title </strong>" + ourMusic[i].title + "<br>" + "<strong>Release year: </strong>" + 
+                                              ourMusic[i].release_year +"<br><strong>Formats: </strong>" + ourMusic[i].formats + "<br></div>";
+  }
+}
+
+printOurMusic();
+

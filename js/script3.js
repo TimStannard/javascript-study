540e3a030b70fdc4546eba18fe905f2dfbf5c303
Unknown <stannard.tim@gmail.com>
2020-02-11 20:27:22 +1300

    Added gold message in boolean condtion

--- js/script3.js
+++ js/script3.js
@@ -31,6 +31,16 @@ var ourMusic = [
       "LP"
     ],
     "gold": false
+  },
+     {
+    "artist": "Shanna lua",
+    "title": "Brazilian Rhythms",
+    "release_year": 1999,
+    "formats": [ 
+      "CD", 
+      "Vinyl"
+    ],
+    "gold": true
   }
 ];
 
@@ -42,12 +52,19 @@ function printOurMusic(){
     // add spaces after commas in the formats 
     ourMusic[i].formats = ourMusic[i].formats.toString();
     ourMusic[i].formats = ourMusic[i].formats.replace(/,/g, ", ");
+    // show gold message if gold
+    function showGoldStatus(){
+    if(ourMusic[i].gold){
+        return "<br><strong>Certified Gold </strong>";
+        }
+        return "";
+    }
     // show all music loop
     document.getElementById("record").innerHTML += "<div class='box'><h3>Record "+ (i+1) + "</h3><br>" + "<strong>Artist </strong>" + ourMusic[i].artist +
                                               "<br><strong>Title </strong>" + ourMusic[i].title + "<br>" + "<strong>Release year: </strong>" + 
-                                              ourMusic[i].release_year +"<br><strong>Formats: </strong>" + ourMusic[i].formats + "<br></div>";
-  }
+                                              ourMusic[i].release_year +"<br><strong>Formats: </strong>" + ourMusic[i].formats + showGoldStatus();
+    
+    }
 }
-
 printOurMusic();
 
